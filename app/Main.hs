adbcc5dabe12247700b8966b73dab29ac14fa7df
Henrique Fantato <henrique.fsa98@gmail.com>
2022-10-17 20:52:25 -0400

    new random generator for best accuracy to the BMI problem

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ import Control.Monad
 import Control.Monad.Random
 --import Data.Vector.Storable (basicLength)
 import Data.List
+import GHC.Float
 import Data.Maybe
 import Numeric.LinearAlgebra
 import System.Environment
@@ -31,11 +32,21 @@ data Network :: * where
           -> Network
 infixr 5 :&~
 
-linear :: Floating a => a -> a
+instance Show Network where        -- Implementacao de instancia de show de Network para facilitar o debug
+  show (O a) = "Nos: " ++ show (wNodes a) ++ ", Pesos: " ++ show (wBiases a)
+  show (a :&~ b) = "Nos: " ++ show (wNodes a) ++ ", Pesos: " ++ show (wBiases a) ++ "\n" ++ show b
+
+linear :: Floating a => a -> a   -- tende a explodir o valor maximo cabivel de um double...
 linear x = x
 
 linear' :: Floating (Vector Double) => Vector Double -> Vector Double
 linear' x = Numeric.LinearAlgebra.fromList $ replicate (size  x) 1
+
+tangente :: Floating a => a -> a 
+tangente x = ((exp x) - (exp (-x))) / (((exp x) + (exp (-x))))
+
+tangente' :: Floating a => a -> a 
+tangente' x = 1 + ((tangente x) * (tangente x))
 
 logistic :: Floating a => a -> a
 logistic x = 1 / (1 + exp (-x))
@@ -54,11 +65,17 @@ runNet (w :&~ n') !v = let v' = logistic (runLayer w v)
                        in  runNet n' v'
 
 
-runNetCustom :: Network -> (Floating (Vector Double) => Vector Double -> Vector Double) -> Vector Double -> Vector Double  -- para usar funcs de ativacao customizadas
-runNetCustom (O w)      f !v = f (runLayer w v)
-runNetCustom (w :&~ n') f !v = let v' = f (runLayer w v)
-                       in  runNet n' v'
 
+runNetCustom :: Network -> (Floating (Vector Double) => Vector Double -> Vector Double) -> Vector Double -> Vector Double  -- para usar funcs de ativacao customizadas
+runNetCustom (O w)      f !v = f (runLayer w v)  -- era f, agora forcando que a ultima camada seja logistica!
+runNetCustom (w :&~ n') f !v = let v' = f (runLayer w v)
+                       in  runNetCustom n' f v'   -- mudar para runNetCustom
+
+
+vectorRandomico x = do
+                      seed :: Int <- getRandom
+                      let vec = randomVector seed Uniform x
+                      return vec
 
 randomWeights :: MonadRandom m => Int -> Int -> m Weights
 randomWeights i o = do
@@ -128,7 +145,7 @@ trainCustom rate x0 target net f f' = fst $ go x0 net
     -- handle the output layer
     go !x (O w@(W wB wN))
         = let y    = runLayer w x
-              o    = f y
+              o    = f y     -- era f, tentando for'car agora que a ultima camada apenas seja logistica, e as demais possam ser F
               -- the gradient (how much y affects the error)
               --   (logistic' is the derivative of logistic)
               dEdy = f' y * (o - target)
@@ -165,13 +182,13 @@ netTest rate n = do
                       || v `inCircle` (fromRational (-0.33), 0.33)
                    then fromRational 1
                    else fromRational 0
-    net0 <- randomNet 2 [16,8] 1
+    net0 <- randomNet 2 [4,3] 1     -- params originais: 2 [16,8] 1
     let trained = foldl' trainEach net0 (zip inps outs)
           where
             trainEach :: Network -> (Vector Double, Vector Double) -> Network
             trainEach nt (i, o) =  trainCustom rate i o nt logistic logistic'  -- train rate i o nt
 
-        outMat = [ [ render (norm_2 (runNet trained (vector [x / 25 - 1,y / 10 - 1])))
+        outMat = [ [ render (norm_2 (runNet trained (vector [x / 25 - 1,y / 10 - 1])))   
                    | x <- [0..50] ]    -- init v 50
                  | y <- [0..20] ]      -- init v 20
         render r | r <= 0.2  = ' '
@@ -186,33 +203,51 @@ netTest rate n = do
     v `inCircle` (o, r) = norm_2 (v - o) <= r
 
 
-imc vec = if (atIndex vec 0 / (a * a)) >= 25.0 then Numeric.LinearAlgebra.fromList [1.0 :: Double] else Numeric.LinearAlgebra.fromList [0.0 :: Double] -- retorna 1 se acima do peso 
+imc3 _ = fromRational 1
+
+imc2 vec = if (atIndex vec 0 / (a * a)) >= 25.0 then Numeric.LinearAlgebra.fromList [1.0 :: Double] else Numeric.LinearAlgebra.fromList [0.0 :: Double] -- retorna 1 se acima do peso 
+            where
+              a = atIndex vec 1
+
+imc vec = if (atIndex vec 0 / (a * a)) >= 25.0 then fromRational 1 else fromRational 0 -- retorna 1 se acima do peso 
             where
               a = atIndex vec 1
 
 
-netTest2 :: MonadRandom m => Double -> Int -> m String     -- Tentativa de resolver IMC
+randomNumber lo hi gen = head $ randomRs (lo, hi) gen
+
+randomNumberPrint lo hi = do
+                g <- newStdGen
+                let result :: Double = randomNumber lo hi g
+                return result
+
+
+netTest2 :: (MonadRandom m, MonadIO m) => Double -> Int -> m (String, Network)     -- Tentativa de resolver IMC
 netTest2 rate n = do
     inps <- replicateM n $ do
-      s <- getRandom
-      return $ randomVector s Uniform  2 * 2 - 1
+      --s <- getRandom
+      g <- newStdGen
+      let randomHeight :: Double = randomNumber 1.0 2.5 g
+      let randomWeight :: Double = randomNumber 35.0 250.0 g
+      --return $ randomVector s (enumFromThen initLimit endLimit)  2 * 2 - 2
+      return $ Numeric.LinearAlgebra.fromList $ [randomHeight, randomWeight]
 
     --let outs = {-map Numeric.LinearAlgebra.fromList $-} map ((\x -> [x]) . imc) inps
     let outs = {-map Numeric.LinearAlgebra.fromList $-} map imc inps
 
-    net0 <- randomNet 2 [16,8] 1 -- aparentemente tem problemas em ter mais de um output  -- nao tinha problemas com mais de um output, mas sim com a func de ativi
+    net0 <- randomNet 2 [16, 8, 4] 1 -- params originais: 2 [16,8] 1 -- aparentemente tem problemas em ter mais de um output  -- nao tinha problemas com mais de um output, mas sim com a func de ativi
 
     let trained = foldl' trainEach net0 (zip inps outs)
           where
             trainEach :: Network -> (Vector Double, Vector Double) -> Network
             trainEach nt (i, o) = trainCustom rate i o nt logistic logistic'
 
-        outMat = [ [ render (( {-norm_2 -} (runNet trained (vector [x,y]))), x, y)
+        outMat = [ [ render (( {-norm_2-}  (runNetCustom trained logistic (vector [x,y]))), x, y)     -- usando runNetCustom para poder passar func activ custom pra rodar a net
                    | x <- ([45,46 .. 150]) ]    -- init v 50    -- pesos
                  | y <- ([1.00, 1.05 .. 2.15])]      -- init v 20    -- alturas
         render (result, p, a) = "peso: " ++ show p ++ ", altura: " ++ show a ++ ", imc: " ++ show (imc (Numeric.LinearAlgebra.fromList[p,a])) ++ ", AI Result: " ++ show result
 
-    return $ unlines $ map unlines outMat
+    return $ (unlines $ map unlines outMat, trained)
 
 
 main :: IO ()
@@ -221,9 +256,15 @@ main = do
     let n    = readMaybe =<< (args !!? 0)
         rate = readMaybe =<< (args !!? 1)
     putStrLn "Training network..."
-    putStrLn =<< evalRandIO (netTest2 (fromMaybe 0.25   rate)
+    (outputS, netTrained) <- (netTest2 (fromMaybe 0.25   rate)
                                      (fromMaybe 500000 n   )   -- init value 500000
                             )
+    putStrLn outputS
+    putStrLn "\n\n\nAgora imprimindo a rede final:\n"
+    putStrLn $ show netTrained
+    {-putStrLn =<< evalRandIO (netTest2 (fromMaybe 0.25   rate)
+                                     (fromMaybe 500000 n   )   -- init value 500000
+                            )-}
 
 (!!?) :: [a] -> Int -> Maybe a
 xs !!? i = listToMaybe (drop i xs)
